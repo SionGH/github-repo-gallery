ded79ee6a158346d468acd72b9ccc27a7b9ff16a
Sion <siontee@gmail.com>
2022-11-13 12:27:52 +0000

    Changes js1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const profile = document.querySelector(".overview");
+const username = "SionGH";
+
+const gitProfileInfo = async function () {
+    const profileInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await profileInfo.json();
+displayProfileInfo(data);
+};
+gitProfileInfo();
+
+const displayProfileInfo = function(data){
+    const div = document.createElement( "div");
+    div.classList.add ("user-info");
+    div.innerHTML =   `
+    <figure>
+    <img alt="user avatar" src=${data.avatar_url} />
+  </figure>
+  <div>
+    <p><strong>Name:</strong> ${data.name}</p>
+    <p><strong>Bio:</strong> ${data.bio}</p>
+    <p><strong>Location:</strong> ${data.location}</p>
+    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+  </div> 
+  `;
+profile.append(div);
+};
