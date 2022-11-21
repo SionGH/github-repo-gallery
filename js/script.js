9ade2248b39a4094b12cb6a553b718aeb2abe2a5
Sion <siontee@gmail.com>
2022-11-21 12:38:05 +0000

    Changes js2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const profile = document.querySelector(".overview");
 const username = "SionGH";
 const repoList = document.querySelector(".repo-list");
-const repos = document.querySelector(".repos");
+const allRepos = document.querySelector(".repos");
 const repoData = document.querySelector(".repo-data");
+const backToRepo = document.querySelector(".view-repos");
+const filterInput = document.querySelector(".filter-repos");
 
 const gitProfileInfo = async function () {
     const profileInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -26,14 +28,15 @@ const displayProfileInfo = function(data){
   </div> 
   `;
 profile.append(div);
-gitRepoInfo();
+gitRepoInfo(username);
 };
-const gitRepoInfo = async function () {
+const gitRepoInfo = async function (username) {
     const getRepoInfo = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
     const repoData = await getRepoInfo.json();
     displayGetRepoInfo(repoData);
 }; 
 const displayGetRepoInfo = function(repos){
+  filterInput.classList.remove("hide");
     for (const repo of repos) {
      const listItem = document.createElement("li");
      listItem.classList.add ("repo");
@@ -41,7 +44,7 @@ const displayGetRepoInfo = function(repos){
      repoList.append(listItem);
     }
 };
-repoList.addEventListener("click",function(e) {
+repoList.addEventListener("click", function (e) {
   if (e.target.matches("h3")) {
     const repoName = e.target.innerText;
     getRepoInformation(repoName);
@@ -66,12 +69,13 @@ displaySpecificRepoInfo(repoInfo, languages);
 
 
 const displaySpecificRepoInfo = function (repoInfo, languages) {
+ backToRepo.classList.remove("hide");
  repoData.innerHTML = "";
  repoData.classList.remove("hide");
- repos.classList.add("hide");
+ allRepos.classList.add("hide");
  const div = document.createElement("div");
  div.innerHTML = `
- <h3>Name: ${repoInfo.name}</h3>
+    <h3>Name: ${repoInfo.name}</h3>
     <p>Description: ${repoInfo.description}</p>
     <p>Default Branch: ${repoInfo.default_branch}</p>
     <p>Languages: ${languages.join(", ")}</p>
@@ -79,3 +83,26 @@ const displaySpecificRepoInfo = function (repoInfo, languages) {
   `;
   repoData.append(div);
   };
+backToRepo.addEventListener("click", function() {
+ allRepos.classList.remove ("hide");
+ repoData.classList.add ("hide");
+ backToRepo.classList.add("hide");
+  });
+
+filterInput.addEventListener = ("input", function (e) {
+  const searchText = e.target.value;
+  const repos = document.querySelectorAll(".repo");
+  const searchLowerText = searchText.toLowerCase();
+
+  for(const repo of repos) {
+    const repoLowerText = repo.innerText.toLowerCase();
+    if (repoLowerText.includes(searchLowerText)) {
+      repo.classList.remove("hide");
+    }  else  {
+      repo.classList.add("hide");
+    }
+  }
+});
+
+  
+
